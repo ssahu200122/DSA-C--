5421653ef656a9a6dc3dc57b7acfad140c8c6668
ssahu200122 <ssahu200122@gmail.com>
2025-11-12 15:50:47 +0530

    Solved book allocation problem using binary search

--- BookAllocationProblem.cpp
+++ BookAllocationProblem.cpp
@@ -29,9 +29,28 @@ int bookallocationProblemLinearSearch(vector<int> books,int students){
     }
 }
 
+int bookAllocationProblemBinarySearch(vector<int> books,int students){
+    int low = *max_element(books.begin(),books.end());
+    long high = accumulate(books.begin(),books.end(),0L);
+    int ans;
+    while(low<=high){
+        int mid = (low+high)/2;
+        int required = numberOFStudent(books,mid);
+        if(students == required) {
+            ans = mid;
+            high = mid-1;
+        }
+        else if(students > required) high = mid-1;
+        else low = mid+1;
+    }
+    return ans;
+    
+}
+
 int main() {
     
     vector<int> books = {25,46,28,49,24};
-    cout<< bookallocationProblemLinearSearch(books,4);
+    cout<< bookallocationProblemLinearSearch(books,4)<<endl;
+    cout<< bookAllocationProblemBinarySearch(books,4);
     return 0;
 }
